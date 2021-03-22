4342fde2cb9b195258a049cfe225ff671a32bf0a
tanglu <wasltds@163.com>
2021-03-22 23:16:52 +0800

    新增页面

---
+++ miniprogram/custom-tab-bar/index.js
@@ -0,0 +1,30 @@
+Component({
+  data: {
+    selected: 0,
+    "color": "#333",
+    "selectedColor": "#3A96E7",
+    "backgroundColor": "#ffffff",
+    "borderStyle": "black",
+    "list": [{
+      "pagePath": "/pages/index/index",
+      "text": "文化世界"
+    }, {
+      "pagePath": "/pages/movie/movie",
+      "text": "电影世界"
+    }, {
+      "pagePath": "/pages/mine/mine",
+      "text": "个人世界"
+    }]
+  },
+  attached() {},
+  methods: {
+    switchTab(e) {
+      const data = e.currentTarget.dataset
+      const url = data.path
+      wx.switchTab({url})
+      this.setData({
+        selected: data.index
+      })
+    }
+  }
+})
